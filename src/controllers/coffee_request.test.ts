f4783f11371197db2942ddd9c5f49ac3bc75b491
neema <neemadas@gmail.com>
2023-03-02 02:56:17 +0000

    test: Add test to check if GET /coffee with parameter return correct object

--- src/controllers/coffee_request.test.ts
+++ src/controllers/coffee_request.test.ts
@@ -9,4 +9,20 @@ describe("test to check coffee endpoint", () => {
     expect(res.statusCode).toEqual(200);
     expect(res.body).toEqual({ drinkType: "Coffee", name: "Latte" });
   });
+
+  test("GET /coffee with default parameter should return correct object", async () => {
+    const res = await request(app)
+      .get("/coffee")
+      .query({ coffeeName: "Latte" });
+    expect(res.statusCode).toEqual(200);
+    expect(res.body).toEqual({ drinkType: "Coffee", name: "Latte" });
+  });
+
+  test("GET /coffee with parameter should return correct object", async () => {
+    const res = await request(app)
+      .get("/coffee")
+      .query({ coffeeName: "Cappuccino" });
+    expect(res.statusCode).toEqual(200);
+    expect(res.body).toEqual({ drinkType: "Coffee", name: "Cappuccino" });
+  });
 });
